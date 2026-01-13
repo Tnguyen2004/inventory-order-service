7d787c4c9c91144c5abb07ae4309d50faa6060d8
Thomas Nguyen <thomasnguyen15462@gmail.com>
2026-01-12 19:49:29 -0500

    Install postgres + prisma

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
@@ -7,6 +8,15 @@ app.use(express.json());
 app.get('/health', (req, res) => {
     res.status(200).send({ status: 'OK' });
 })
+
+app.get('/health/db', async (req, res) => {
+    try {
+        await prisma.$queryRaw`SELECT 1`;
+        res.status(200).send({ database: 'OK' });
+    } catch (error) {
+        res.status(500).send({ database: 'ERROR', details: error });
+    }
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
